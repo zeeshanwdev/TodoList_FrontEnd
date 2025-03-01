37e3fc0b599f926aa010534a27f718b6563d668d
M.Zeeshan <zeeshanktkextra@gmail.com>
2025-03-02 03:33:42 +0500

    second commit

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SessionWrapper from "./components/sessionWrapper.js";
+import Script from "next/script";  // Import Script from Next.js
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,11 +28,14 @@ export default function RootLayout({ children }) {
         <Navbar />
 
         <div className="min-h-[66vh] ">            
-        <div class="absolute top-0 -z-10 h-full w-full bg-white"><div class="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div></div>
-          <script src="https://cdn.lordicon.com/lordicon.js"></script>             {/* LordIcon Script*/}
+          <div className="absolute top-0 -z-10 h-full w-full bg-white">
+            <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div>
+          </div>
+          
+          {/* LordIcon Script using Next.js Script component */}
+          <Script src="https://cdn.lordicon.com/lordicon.js" strategy="beforeInteractive" />
 
           {children}
-          
         </div>
 
         <Footer/>
@@ -40,8 +44,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-
-
-
-
-// 
